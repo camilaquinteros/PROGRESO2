2a8f490ad6ef9a1ffceac85ab5e434263b88ab6e
camilaquinteros <130692640+camilaquinteros@users.noreply.github.com>
2023-05-18 11:54:51 -0500

    Add files via upload

---
+++ diagonaldelamatriz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int filas, columnas;
+    
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &filas);
+    
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &columnas);
