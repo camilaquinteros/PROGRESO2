74bd9f929c5b4ac4bff485c59c7d721f606368af
camilaquinteros <130692640+camilaquinteros@users.noreply.github.com>
2023-05-28 18:21:00 -0500

    Update Tallercolaborativo.c

--- Tallercolaborativo.c
+++ Tallercolaborativo.c
@@ -22,3 +22,27 @@ int main() {
         {2022, {160, 180, 120, 140, 120, 100, 170}},
         {2023, {180, 190, 130, 150, 130, 110, 190}}
     };
+ 
+    int num_anios = sizeof(datos_alumnos) / sizeof(DatosAlumnos);
+
+ 
+
+    // a) Calcular el año en que ingresó la mayor cantidad de alumnos a la universidad
+    int max_cantidad_total = 0;
+    int anio_max_cantidad_total = 0;
+
+ 
+
+    for (int i = 0; i < num_anios; i++) {
+        int cantidad_total = 0;
+        for (int j = 0; j < NUM_CARRERAS; j++) {
+            cantidad_total += datos_alumnos[i].cantidad[j];
+        }
+        if (cantidad_total > max_cantidad_total) {
+            max_cantidad_total = cantidad_total;
+            anio_max_cantidad_total = datos_alumnos[i].anio;
+        }
+    }
+ 
+ 
+ 
